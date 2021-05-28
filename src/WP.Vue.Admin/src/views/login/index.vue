0a11f2db6345fc72f7754dd784bf34b2bc44bc41
w405287435 <648779619@qq.com>
2021-05-29 00:01:04 +0800

    调整登录页

--- src/WP.Vue.Admin/src/views/login/index.vue
+++ src/WP.Vue.Admin/src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">Login </h3>
       </div>
 
       <el-form-item prop="username">
@@ -41,7 +41,7 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
       <div class="tips">
         <span style="margin-right:20px;">username: admin</span>
@@ -174,8 +174,8 @@ $cursor: #fff;
 
   .el-form-item {
     border: 1px solid #454545;
-    // background: rgba(0, 0, 0, 0.7);
-    background: #fff;
+    background: rgba(0, 0, 0, 0.7);
+    // background: #;
     border-radius: 5px;
     color: #454545;
   }
@@ -233,7 +233,7 @@ $light_gray:#eee;
 
     .title {
       font-size: 26px;
-      color: #000;
+      color: #fff;
       margin: 0px auto 40px auto;
       text-align: center;
       font-weight: bold;
